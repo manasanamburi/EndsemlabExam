04f4c57911873d35bf7f4b76cfbdc417e7d45519
manasanamburi <manasanamburi7013280599@gmail.com>
2025-11-28 18:51:56 +0530

    Add files via upload

---
+++ app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CourseRegistration from './CourseRegistration'; 
+function App() {
+  return (
+    <div className="App">
+      <h1>University Portal</h1>
+      <CourseRegistration />
+    </div>
+  );
+}
+
+export default App;
